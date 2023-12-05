8ec58c36f4b58202b196cd710df5f9ea0a8aa2d0
pbrocketmoj <85356666+pbrocketmoj@users.noreply.github.com>
2023-12-05 16:49:03 +0000

    V5 testing (#85)
    * adding v5
    
    * new booking pages
    
    * added first unhappy path, begin attempt at homepage, accordion date picker

--- app/routes.js
+++ app/routes.js
@@ -16,10 +16,12 @@ router.post('/register-another-visitor-answer', function (req, res) {
     // Check whether the variable matches a condition
     if (registerAnother == "yes"){
       // Send user to next page
-      res.redirect('online-booking/register-to-visit-v4/visitor-name')
+      res.redirect('online-booking/register-to-visit-v5/4visitor-name')
     } else {
       // Send user to ineligible page
-      res.redirect('online-booking/register-to-visit-v4/visitor-check-answers')
+      res.redirect('online-booking/register-to-visit-v5/4visitor-check-answers')
     }
   
   })
+
+ 
