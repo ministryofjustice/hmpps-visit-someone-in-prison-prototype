f264830498761e5870fe312aa2ed26273da921b9
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-12-07 22:06:19 +0000

    Add calendar date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,26 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+
+  if (document.getElementById('calendar-slot-picker')) {
+
+    function makeDateActive(event) {
+      event.preventDefault()
+      const dateLink = event.target
+
+      document.querySelectorAll('.calendar-slots__active').forEach(e => e.classList.remove('calendar-slots__active'))
+      document.querySelectorAll('.day-slots__active').forEach(e => e.classList.remove('day-slots__active'))
+
+      dateLink.parentElement.classList.add('calendar-slots__active')
+      document.querySelector(dateLink.hash).classList.add('day-slots__active')
+      document.querySelector('.calendar-slot-button').classList.add('calendar-slot-button__active')
+    }
+
+    document.querySelectorAll('.calendar-slots a').forEach(dateLink => {
+      dateLink.addEventListener('click', makeDateActive)
+    })
+  }
+
 })
 
 window.MOJFrontend.initAll()
