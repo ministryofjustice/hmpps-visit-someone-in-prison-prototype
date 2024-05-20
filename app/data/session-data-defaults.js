f02a86babac4a9005b488b85d6f11da4e0b2cbfc
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-05-20 17:33:18 +0100

    Update session-data-defaults.js

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -5,6 +5,7 @@ module.exports = {
   'prisoner-name': 'Jane Smith',
   'visitor-1-name': 'Susan Smith',
   'visitor-1-age': '39',
+  'visitor-1-tel': '0789085627'
   'visitor-2-name': 'Michael Smith',
   'visitor-2-age': '15'
   
