bd32e539789fafd40d71d5941a3cc0b30f33972b
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-05-20 17:09:21 +0100

    Default session data (#166)
    * Update session-data-defaults.js
    
    * Update index.html
    
    * Update 1-visitors.html
    
    * Update 4-main-contact.html
    
    * Update 5-check-answers.html
    
    * Update details.html
    
    * Update past-details.html
    
    * Update cancelled-details.html

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,6 +1,11 @@
 module.exports = {
 
   'telephone-number': '0121 661 2101',
-  'prison-name': 'Foston Hall'
-
+  'prison-name': 'Foston Hall',
+  'prisoner-name': 'Jane Smith',
+  'visitor-1-name': 'Susan Smith',
+  'visitor-1-age': '39',
+  'visitor-2-name': 'Michael Smith',
+  'visitor-2-age': '15'
+  
 }
