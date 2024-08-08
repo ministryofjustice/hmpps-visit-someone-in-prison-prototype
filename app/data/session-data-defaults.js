ebc7efc3d0f50de6bfb0d0615109db75fa3629fe
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-08-08 20:27:30 +0000

    added more default visitors and reinsated visitor area

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -5,8 +5,15 @@ module.exports = {
   'prisoner-name': 'Jane Smith',
   'visitor-1-name': 'Susan Smith',
   'visitor-1-age': '39',
+  'visitor-1-dob': '12 March 1987',
   'visitor-1-tel': '0789085627',
   'visitor-2-name': 'Michael Smith',
-  'visitor-2-age': '15'
-  
+  'visitor-2-age': '34',
+  'visitor-2-dob': '2 June 1990',
+  'visitor-3-name': 'Jamie Smith',
+  'visitor-3-age': '29',
+  'visitor-3-dob': '10 October 1995',
+  'visitor-4-name': 'Neil Smith',
+  'visitor-4-age': '24',
+  'visitor-4-dob': '1 January 2000'
 }
