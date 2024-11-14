82ee50bf4506753d07ce261db424f6d6d75d3f69
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-11-14 14:05:00 +0000

    Revert "data fixes (#274)" (#275)
    This reverts commit f758b531d83575711edc9b6fa12fa850d83b2d65.

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,50 +1,32 @@
 module.exports = {
 
-  'prison-telephone-number': '0121 661 2101',
+  'telephone-number': '0121 661 2101',
   'prison-name': 'Foston Hall (HMP & YOI)',
-  'prison-url': '{{ data['prison-url'] }}',
   'prisoner-name': 'Jane Smith',
-
   'visitor-1-name': 'Susan Smith',
-  'visitor-1-first-name': 'Susan',
-  'visitor-1-surname': 'Smith',
   'visitor-1-age': '39',
   'visitor-1-dob': '12 March 1987',
   'visitor-1-tel': '0789085627',
   'visitor-1-email': 'susan.smith@email.com',
-
   'visitor-2-name': 'Michael Smith',
-  'visitor-2-first-name': 'Michael',
-  'visitor-2-surname': 'Smith',
   'visitor-2-age': '34',
   'visitor-2-dob': '2 June 1990',
   'visitor-2-tel': '07457896547',
   'visitor-2-email': 'mike.smith@email.com',
-
   'visitor-3-name': 'Jamie Smith',
-  'visitor-3-first-name': 'Jamie',
-  'visitor-3-surname': 'Smith',
   'visitor-3-age': '29',
   'visitor-3-dob': '10 October 1995',
   'visitor-3-tel': '079998745987',
   'visitor-3-email': 'jamie.smith@email.com',
-
   'visitor-4-name': 'Neil Smith',
-  'visitor-4-first-name': 'Neil',
-  'visitor-4-surname': 'Smith',
   'visitor-4-age': '24',
   'visitor-4-dob': '1 January 2000',
-
-
   'visit-0-date': 'Thursday 7 November 2024',
   'visit-0-time': '10am to 11am',
   'visit-0-ref': 'xg-vw-va-fm',
-
-
   'visit-2-date': 'Monday 28 September 2024',
   'visit-2-time': '10am to 11am',
   'visit-2-ref': 're-gy-wa-qk',
-
   'add-a-visitor-url': 'https://visit-someone-in-prison-add-visitor.form.service.justice.gov.uk/',
   'feedback-url': 'https://visit-someone-in-prison-test-feedback.form.service.justice.gov.uk/'
 }
