917a8a9675bd6d29a0b06bd268e77524ec77959e
f-marry <fabien.marry@digital.justice.gov.uk>
2025-05-21 15:09:05 +0100

    Updating calendar so selecting a date scrolls to session area

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,19 +12,27 @@ window.GOVUKPrototypeKit.documentReady(() => {
       event.preventDefault()
       const dateLink = event.target
 
+
+//Mark any other date/session as inactive
       document.querySelectorAll('.calendar-slots__active').forEach(e => e.classList.remove('calendar-slots__active'))
       document.querySelectorAll('.day-slots__active').forEach(e => e.classList.remove('day-slots__active'))
-
+      
+//Mark the selected date/session as active
       dateLink.parentElement.classList.add('calendar-slots__active')
       document.querySelector(dateLink.hash).classList.add('day-slots__active')
       document.querySelector('.calendar-slot-button').classList.add('calendar-slot-button__active')
+//scrolls the page to the session selection block for the selected date.
+      
+      dateLink.scrollIntoView({ behavior: 'smooth' });
+
     }
 
     document.querySelectorAll('.calendar-slots a').forEach(dateLink => {
       dateLink.addEventListener('click', makeDateActive)
     })
+  
+  
   }
-
 })
 
 window.MOJFrontend.initAll()
