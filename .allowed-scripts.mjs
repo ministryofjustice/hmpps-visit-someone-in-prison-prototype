1f971cd433b3d9653eb87777e8ccc899eeded0b3
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2026-04-17 10:27:16 +0100

    Add security stuff (#617)

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,9 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+   allowlist: {
+      'node_modules/govuk-prototype-kit/node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+      'node_modules/govuk-prototype-kit/node_modules/fsevents@2.3.2': 'ALLOW',
+      'node_modules/hmrc-frontend@7.9.0': 'ALLOW'
+   },
+})
