cf61f2aa20e5c9e45e8622f9475272623e2be3b2
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2026-04-22 14:52:37 +0100

    Update .allowed-scripts.mjs (#626)

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,6 +4,6 @@ export default configureAllowedScripts({
    allowlist: {
       'node_modules/govuk-prototype-kit/node_modules/@parcel/watcher@2.5.1': 'ALLOW',
       'node_modules/govuk-prototype-kit/node_modules/fsevents@2.3.2': 'ALLOW',
-      'node_modules/hmrc-frontend@7.9.0': 'ALLOW'
+      'node_modules/hmrc-frontend@7.11.0': 'ALLOW'
    },
 })
