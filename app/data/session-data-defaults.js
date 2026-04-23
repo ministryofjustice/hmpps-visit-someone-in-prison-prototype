ae69a2aac7dedd11cf5f156cdb6aa8d30c92ddc0
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2026-04-23 16:46:34 +0100

    new service name (#627)

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  'app-name': 'Manage prison visits',
+  'app-name': 'Social visits',
   'prison-tel': '0121 661 2101',
   'prison-name': 'Foston Hall (HMP & YOI)',
   'prison-url': 'https://www.gov.uk/guidance/foston-hall-prison',
